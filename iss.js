62009de5d59987be82bb9cdb79d6253a0a094fd1
sselinatoledo <sselinatoledo@gmail.com>
2023-05-04 12:46:10 +0000

    add iss.js

--- iss.js
+++ iss.js
@@ -1,19 +1,22 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+const fetchCoordsByIP = function(ip, callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
-  });
-};
+    const parsedBody = JSON.parse(body);
+    if (!parsedBody.success) {
+        const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+        callback(Error(message), null);
+        return;
+      } 
+  
+      const { latitude, longitude } = parsedBody;
+  
+      callback(null, {latitude, longitude});
+    });
+  };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
