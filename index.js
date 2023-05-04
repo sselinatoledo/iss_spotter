93f0abe6eb834709b02c7cd07d310add01e582d0
sselinatoledo <sselinatoledo@gmail.com>
2023-05-04 12:44:58 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
